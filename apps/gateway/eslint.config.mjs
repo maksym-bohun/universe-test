86d6a9c9b1dc22fb4effd1ad9dcfa5baa280476e
maksym_bohun <maxim.boggun@gmail.com>
2025-06-06 00:43:11 +0300

    improve: changed eslint config and fixed issue with starting reporter

--- apps/gateway/eslint.config.mjs
+++ apps/gateway/eslint.config.mjs
@@ -1,34 +1,50 @@
-// @ts-check
-import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import tsParser from '@typescript-eslint/parser';
+import tsEslintPlugin from '@typescript-eslint/eslint-plugin';
+import prettier from 'eslint-plugin-prettier';
+import importPlugin from 'eslint-plugin-import';
 import globals from 'globals';
-import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
-  {
-    ignores: ['eslint.config.mjs'],
-  },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
-      sourceType: 'commonjs',
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
+const typescriptOptions = {
+  files: ['**/*.ts'],
+  languageOptions: {
+    parser: tsParser,
+    parserOptions: {
+      project: ['tsconfig.json'],
+    },
+    sourceType: 'module',
+    ecmaVersion: 'latest',
+    globals: {
+      ...globals.node,
     },
   },
-  {
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
-    },
+  plugins: {
+    '@typescript-eslint': tsEslintPlugin,
+    prettier,
+    import: importPlugin,
   },
-);
+  rules: {
+    'prettier/prettier': 'error',
+
+    '@typescript-eslint/no-floating-promises': 'error',
+    '@typescript-eslint/no-misused-promises': [
+      'error',
+      { checksVoidReturn: false },
+    ],
+
+    'no-redeclare': 'off',
+    '@typescript-eslint/no-redeclare': 'off',
+
+    'import/no-unresolved': 'off',
+    'import/no-extraneous-dependencies': 'off',
+
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: "CallExpression[callee.name='ManyToMany']",
+        message: 'ManyToMany decorator is restricted.',
+      },
+    ],
+  },
+};
+
+export default [typescriptOptions];
